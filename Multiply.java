3e30537f9d8325eebce13edd8c9bcacdd7279cec
emozailin4 <32354329+emozailin4@users.noreply.github.com>
2018-11-02 23:27:05 +0800

    Update Multiply.java
    完成了这个生成乘法表的算法

--- Multiply.java
+++ Multiply.java
@@ -2,16 +2,18 @@ package scannerUse;
 import java.util.Scanner;
 public class Multiply {
 	public void MultiplyTable(int MaxNumber) {
-		boolean IsTrue = false;
-		if((MaxNumber>=1)||(MaxNumber<=9)){
-			isTrue = ture;
-			
-			if(int i = 0 ; i < MaxNumber; i++){
-				
+		int BeginNumber = 1;
+		int MaxBeginNumber = 1;
+		if((MaxNumber>=1)&(MaxNumber<=9)){//对输入的数字进行判断
+			for(MaxBeginNumber = 1;MaxBeginNumber <= MaxNumber; MaxBeginNumber++) {//外部循环
+				for(BeginNumber = 1;BeginNumber <= MaxBeginNumber ;BeginNumber++) {//内部循环
+					int Answer = BeginNumber * MaxBeginNumber;//答案
+					System.out.println(BeginNumber+"*"+MaxBeginNumber +"=" +Answer);//打印出来
+				}
 			}
 		}else{
 			System.out.println("请输入范围内的数字");
-			}
+		}
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
